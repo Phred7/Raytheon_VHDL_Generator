cb96cafb934118e68844dc6233647ed4a4593df9
wward <woward00@gmail.com>
2021-12-06 10:45:34 -0700

    assembler and disassembler work

--- assembler.py
+++ assembler.py
@@ -1,5 +1,13 @@
+###############################
+# CCS Disassembler
+# For Raytheon Research Project
+# Dr. Clem Izurieta
+# Dr. Brock LaMeres
+# Written by Walker Ward
+###############################
 import logging
 import os
+from contextlib import contextmanager
 from logging import Logger
 
 
@@ -7,12 +15,60 @@ class Assembler:
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logger: Logger = logging.getLogger(__name__)
     assembler_directory: str = r'C:\ti\ccs1040\ccs\tools\compiler\ti-cgt-msp430_20.2.5.LTS\bin'
-    assembler_executable: str = r'asm430.exe'
+    assembler_executable: str = r'asm430.exe'  # r'asm430.exe' Tested: lc430, lnk430
 
-    def __init__(self, input_file_name: str = ""):
+    def __init__(self, *, input_file_name: str, input_file_directory: str = None,
+                 output_file_name: str = "generated_assembly.out", output_file_directory: str = None):
         self.assembler_input_file_name: str = input_file_name  # "All_msp_operations.out"  # "test_C.out"  # "test_colt_C.out"  # "All_ops_asm.out'  # "Motor_mover_C.out" # "test.out" # "test_ASM.out" # "All_msp_operations.out"
+        self.assembler_output_file_name: str = output_file_name
+        self.assembler_input_file_directory: str = input_file_directory if input_file_directory is not None else rf"{os.getcwd()}\generated_disassembly"
+        self.assembler_output_file_directory: str = output_file_directory if output_file_directory is not None else rf"{os.getcwd()}\generated_assembly"
+        self.file_should_exist(file_directory=self.assembler_input_file_directory, file=self.assembler_input_file_name)
 
-    def assemble(self) -> None:
-        assembler_input_file_directory: str = rf"{os.getcwd()}\ccs_workspace\{self.assembler_input_file_name.replace('.out', '')}\Debug"
-        assembler_output_file_name: str = ""
-        assembler_output_file_directory: str = rf"{os.getcwd()}\x"
+    def assemble(self, *, delete_output_if_exists: bool = True) -> None:
+        # If delete_output_if_exists is True: Check if the output file already exists. If it exists delete in.
+        if delete_output_if_exists:
+            if os.path.exists(rf"{self.assembler_output_file_directory}\{self.assembler_output_file_name}"):
+                os.remove(rf"{self.assembler_output_file_directory}\{self.assembler_output_file_name}")
+                self.logger.info(rf"Removed {self.assembler_output_file_directory}\{self.assembler_output_file_name}")
+
+        # Call the assembler.
+        disassembler_exit_status: int = os.system(
+            rf"{self.assembler_directory}\{self.assembler_executable} {self.assembler_input_file_directory}\{self.assembler_input_file_name} {self.assembler_output_file_directory}\{self.assembler_output_file_name}")
+        self.logger.debug(f"Assembler exit status: {disassembler_exit_status}")
+        if disassembler_exit_status != 0:
+            self.logger.info(
+                f"Assembler failed to generate assembly for {self.assembler_input_file_name} with exit status {disassembler_exit_status}")
+            # raise OSError(f"Assembler failed to generate assembly for {self.assembler_input_file_name} with exit status {disassembler_exit_status}")
+        else:
+            self.logger.info(
+                f'Assembler generated {self.assembler_output_file_name} at the directory {self.assembler_output_file_directory} using {self.assembler_executable}')
+        raise NotImplementedError("The Assembler is not fully functional at this moment. The assembler does not reassemble disassembly. To reassemble we are going to need to scan the disassembly and create a ASM file from that, compile it with CCS's tools and then we should be able to generate .out and .obj files")
+
+    @contextmanager
+    def change_dir(self, destination: str) -> None:
+        cwd: str = ""
+        try:
+            cwd = os.getcwd()
+            os.chdir(destination)
+            yield
+        finally:
+            os.chdir(cwd)
+
+    @staticmethod
+    def file_should_exist(file_directory: str, file: str) -> int:
+        """
+        Returns 0 if file exists. Otherwise raises OSError.
+        :param file_directory: Location of directory containing the File file.
+        :param file: The name of the File file that should exist.
+        :raises OSError: If file does not exist.
+        :return: O if the files exists.
+        """
+        if not os.path.exists(f"{file_directory}\\{file}"):
+            raise OSError(f"{file_directory}\\{file} does not exist")
+        return 0
+
+
+if __name__ == '__main__':
+    raise NotImplementedError
+    #  assembler: Assembler = Assembler()
--- demo.py
+++ demo.py
@@ -1,4 +1,15 @@
+###############################
+# Demo
+# For Raytheon Research Project
+# Dr. Clem Izurieta
+# Dr. Brock LaMeres
+# Written by Michael Heidal and Walker Ward
+###############################
+from typing import Tuple
+
 import package_zipper
+from assembler import Assembler
+from disassembler import Disassembler
 from instrumentation import Instrumentation
 from int_overflow_attack import IntOverflowAttack
 from vhdl_parser_generator import VHDLParserGenerator
@@ -6,11 +17,17 @@ from vhdl_parser_generator import VHDLParserGenerator
 
 class Demo:
 
-    def main(self) -> None:
-        instrumentation: Instrumentation = Instrumentation(IntOverflowAttack())
-        vhdl_parser_generator: VHDLParserGenerator = VHDLParserGenerator()
-        vhdl_parser_generator.generate_vhdl(pique_bin_bool=True)
-        package_zipper.zip_vhdl(zip_file_name="generated_vhdl_instrumentation_test")
+    @staticmethod
+    def main() -> None:
+        """All_msp_operations.out"""
+        disassembler: Disassembler = Disassembler()
+        disassembler_return: Tuple[str, str] = disassembler.disassemble(pique_bool=False)
+        assembler: Assembler = Assembler(input_file_name=disassembler_return[1])
+        assembler.assemble()
+        # instrumentation: Instrumentation = Instrumentation(IntOverflowAttack())
+        # vhdl_parser_generator: VHDLParserGenerator = VHDLParserGenerator()
+        # vhdl_parser_generator.generate_vhdl(pique_bin_bool=True)
+        # package_zipper.zip_vhdl(zip_file_name="generated_vhdl_instrumentation_test")
 
 if __name__ == '__main__':
     demo: Demo = Demo()
