533ab77b683845299273ee8f44be52dbe78174cb
wward <woward00@gmail.com>
2022-02-09 14:14:14 -0700

    more stashed changes

---
+++ ccs_workspace/empty_c_file/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_colt430fr2355.cmd 
+
+C_SRCS += \
+../int_overflow_target.c 
+
+C_DEPS += \
+./int_overflow_target.d 
+
+OBJS += \
+./int_overflow_target.obj 
+
+OBJS__QUOTED += \
+"int_overflow_target.obj" 
+
+C_DEPS__QUOTED += \
+"int_overflow_target.d" 
+
+C_SRCS__QUOTED += \
+"../int_overflow_target.c" 
+
+
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Main:
         """All_msp_operations.out"""
         # instrumentation = Instrumentation(IntOverflowAttack())
         # instrumentation.instrument()
-        vhdl_parser_generator: VHDLParserGenerator = VHDLParserGenerator(binary_file_name="int_overflow_target")
+        vhdl_parser_generator: VHDLParserGenerator = VHDLParserGenerator(binary_file_name="All_msp_operations")
         vhdl_parser_generator.generate_vhdl(detection=True)
         package_zipper: PackageZipper = PackageZipper()
         # TODO: class for ccs project to stop having to pass around file names and paths?
