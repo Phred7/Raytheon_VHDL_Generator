96b6ad621b55be56cd90eba9cb71556a21a951fa
wward <woward00@gmail.com>
2022-04-01 14:47:18 -0600

    modify detection main logging

--- ccs_workspace/test_target/main.c
+++ ccs_workspace/test_target/main.c
@@ -1,19 +1,44 @@
 
+#include <string.h>
+void buff_value(char* target) {
+   strcpy(target, "0000000000000000000000");}
+
+
 #include <msp430.h> 
+
 #include <stdio.h>
+
 /**
+
  * main.c
+
  */
+
 int main(void)
+
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	WDTCTL = WDTPW | WDTHOLD;	
 	char message[8] = "MESSAGE";
+
+char* status_avmbbzuuif;
 	int status = 0;
+;
+char* status_koauannifn;
+buff_value(status_avmbbzuuif);
+buff_value(status_koauannifn);
 	if (status == 0) {
+
 	    printf(message);
+
 	} else {
+
 	    printf("default");
+
 	}
+
 	return 0;
+
 }
+
     
